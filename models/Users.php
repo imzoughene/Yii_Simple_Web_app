b8e0b7df4782aa56b1d1221f28bef44d66bce155
benfarice <imzoughene@outlook.com>
2018-11-20 19:26:15 +0100

    Gii Module CRUD Application

---
+++ models/Users.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $user_id
+ * @property string $username
+ * @property string $email
+ *
+ * @property Posts[] $posts
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'email'], 'required'],
+            [['username', 'email'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'username' => 'Username',
+            'email' => 'Email',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Posts::className(), ['author_id' => 'user_id']);
+    }
+}
